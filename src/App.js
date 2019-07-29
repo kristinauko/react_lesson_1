2d9c9f88101d92981c82603949eb867cadb29a0c
kristina uko <kristinaukosina@gmail.com>
2019-07-29 12:53:16 -0700

    initial commit for functional components practice

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <ul style={listStyle}>
-       <li>Item 1</li>
-       <li>Item 2</li>
-       <li>Item 3</li>
-      </ul>
     </div>
   );
 }
 
-const listStyle = {
-  listStyle: "square"
-}
 export default App;
