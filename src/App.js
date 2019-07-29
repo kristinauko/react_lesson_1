932539a07c42ebccf3283bee04a70b16b28f3ed3
kristina uko <kristinaukosina@gmail.com>
2019-07-29 12:55:56 -0700

    functional components practice

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h1>Kristina</h1>
+        <p>I am learning React</p>
+        <ul style={listStyle}>
+           <li>Vacation 1</li>
+           <li>Vacation 2</li>
+           <li>Vacation 3</li>
+        </ul>
     </div>
   );
 }
 
+const listStyle = {
+  listStyle: "square"
+}
 export default App;
