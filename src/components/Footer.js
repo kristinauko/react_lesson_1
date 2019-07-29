f6855657e46e5dc85e9187d9bc279759278a20f3
kristina uko <kristinaukosina@gmail.com>
2019-07-29 15:32:58 -0700

    parent/child component practice

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>Thanks for visiting!</div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Contacts</li>
+                <li>Links</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
