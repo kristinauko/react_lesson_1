685d5903c2a773801682cf5f496e530eff470409
kristina uko <kristinaukosina@gmail.com>
2019-07-29 16:36:58 -0700

    Todo app: phase 1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+    return(
+        <div>
+            <h1>List</h1>
+            <input type="checkbox"/>
+            <p>Item1</p>
+            <input type="checkbox"/>
+            <p>Item2</p>
+            <input type="checkbox"/>
+            <p>Item3</p>
+        </div>
+    )
+}
+
+export default App
