5ebd544235e25be9c3426602b1e1b95187e35b05
kristina uko <kristinaukosina@gmail.com>
2019-07-28 21:38:33 -0700

    added list, removed style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul style={listStyle}>
+       <li>Item 1</li>
+       <li>Item 2</li>
+       <li>Item 3</li>
+      </ul>
     </div>
   );
 }
 
+const listStyle = {
+  listStyle: "square"
+}
 export default App;
