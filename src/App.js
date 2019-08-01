fe49e056ebb9aa90d793179aab1b22aab7a86e5f
kristina uko <kristinaukosina@gmail.com>
2019-08-01 11:30:59 -0700

    phase 6

--- src/App.js
+++ src/App.js
@@ -10,9 +10,28 @@ class App extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
     }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo     
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
     render() {
-        const todosComponents = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        const todosComponents = this.state.todos.map(item => <TodoItem 
+            key={item.id} 
+            item={item} 
+            handleChange={this.handleChange}/>)
 
         return (
             <div className="todo-list">
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,13 +3,12 @@ import "./component-style.css"
 
 
 function TodoItem(props) {
-    console.log(props)
     return (
     <div className="todo-item">
         <input 
             type="checkbox" 
             checked={props.item.completed} 
-            onChange={() => console.log("changed")}/>
+            onChange={() => props.handleChange(props.item.id)}/>
         <p>{props.item.text}</p>
     </div>
     )
